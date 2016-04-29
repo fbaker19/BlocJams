22651b47f73d5ed7db2baf5cfa03174c7ab02e60
fbaker <fairen.baker@gmail.com>
2016-04-29 14:13:25 -0400

    Dom and nodes

---
+++ scripts/album.js
@@ -0,0 +1,82 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+};
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumPrince = {
+     title: 'Purple Rain',
+     artist: 'Prince',
+     label: 'Warner Bros.',
+     year: '1985',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'When Doves Cry', duration: '5:54' },
+         { title: 'Let\'s Go Crazy', duration: '4:39' },
+         { title: 'Purple Rain', duration: '8:41'},
+         { title: 'I Would Die 4 U', duration: '2:49' },
+         { title: 'Beautiful Ones', duration: '5:13' },
+         { title: 'Computer Blue', duration: '3:59' },
+         { title: 'Darling Nikki', duration: '4:14' },
+         { title: 'Baby I\'m a Star', duration: '4:24' },
+         { title: 'Take Me with U', duration: '3:54'}
+     ]
+ };
+
+var createSongRow = function(songNumber, songName, songLenth){
+    var template = 
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>' ;
+    
+    return template;
+};
+
+var setCurrentAlbum = function(album) {
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     albumSongList.innerHTML = '';
+     
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+ 
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+ };
