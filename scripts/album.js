4a55b751c1f9ebcee60014047a37a490e1df378c
Fairen <fairen.baker@gmail.com>
2016-05-11 18:34:46 -0400

    next/prev song

--- scripts/album.js
+++ scripts/album.js
@@ -124,13 +124,9 @@ var trackIndex = function(album, song){
 };
 
 var nextSong = function(){
-     var getLastSongNumber = function(index){
-         if(index == 0){
-             currentAlbum.songs.length;
-         }else{
-             return index;
-         }
-     };
+    var getLastSongNumber = function(index) {
+        return index == 0 ? currentAlbum.songs.length : index;
+    };
     
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
     currentSongIndex++;
@@ -158,9 +154,38 @@ var nextSong = function(){
     $lastSongNumberCell.html(lastSongNumber);
 };
 
+
 var previousSong = function(){
-     var songNumberCell = $(this).find('.song-item-number');
-     var songNumber = songNumberCell.attr('data-song-number');
+    var getLastSongNumber = function(index) {
+        return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
+    };
+    
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+     
+    currentSongIndex--;
+    
+    if (currentSongIndex < 0) {
+        currentSongIndex = currentAlbum.songs.length - 1;
+    }
+    
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+    
+    var $songTitle = $(".song-name");
+    var $artistName = $(".artist-name");
+    var $musicMobile = $(".artist-song-mobile");
+    
+    $songTitle.text(currentSongFromAlbum.title);
+    $artistName.text(currentAlbum.artist);
+    $musicMobile.text(currentSongFromAlbum.title +" - "+ currentAlbum.artist);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+    
+    var lastSongNumber = getLastSongNumber(currentSongIndex);
+    var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    
+    $nextSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
 };
 
 var updatePlayerBarSong = function() {
@@ -174,17 +199,20 @@ var updatePlayerBarSong = function() {
     $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
-var playButtonTemplate = '<a class="album-song-button"><span class = "ion-play"</span></a>';
-var pauseButtonTemplate = '<a class="album-song-button"><span class = "ion-pause"</span></a>';
-var playerBarPlayButton = '<span class="ion-play"></span>';
-var playerBarPauseButton = '<span class="ion-pause"></span>';
+    var playButtonTemplate = '<a class="album-song-button"><span class = "ion-play"</span></a>';
+    var pauseButtonTemplate = '<a class="album-song-button"><span class = "ion-pause"</span></a>';
+    var playerBarPlayButton = '<span class="ion-play"></span>';
+    var playerBarPauseButton = '<span class="ion-pause"></span>';
 
+    var currentlyPlayingSongNumber = null;
+    var currentAlbum = null;
+    var currentSongFromAlbum = null;
 
-
-var currentlyPlayingSongNumber = null;
-var currentAlbum = null;
-var currentSongFromAlbum = null;
+    var $previousButton = $('.main-controls .previous');
+    var $nextButton = $('.main-controls .next');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
+    $previousButton.click(previousSong);
+    $nextButton.click(nextSong);
   });
